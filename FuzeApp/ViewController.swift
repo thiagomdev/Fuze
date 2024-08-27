4490df864092c5eaf8c53eb7a41988421c03cfc4
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2024-08-27 13:46:00 -0300

    feat: Initial commit app

---
+++ FuzeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FuzeApp
+//
+//  Created by Thiago Monteiro on 27/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
