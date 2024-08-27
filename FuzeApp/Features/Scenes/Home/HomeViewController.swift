72bc83a0a3107be73685ec86498391245f79a08a
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2024-08-27 14:07:35 -0300

    Make setup

--- FuzeApp/Features/Scenes/Home/HomeViewController.swift
+++ FuzeApp/Features/Scenes/Home/HomeViewController.swift
@@ -7,14 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
